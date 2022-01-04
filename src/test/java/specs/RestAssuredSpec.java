b463f41a3b26bb017707cd6699c314b4e207d326
andreikuz <andreikuz@yahoo.com>
2022-01-04 16:33:14 +0300

    first commit

---
+++ src/test/java/specs/RestAssuredSpec.java
@@ -0,0 +1,24 @@
+package specs;
+
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+import static filters.CustomLogFilter.customLogFilter;
+import static io.restassured.RestAssured.with;
+
+    //выносим часто используемые настройки запроса в отдельную спеку и далее добавляем метод в given()/body или через .spec
+    public class RestAssuredSpec {
+        static public RequestSpecification requestSpecification =
+                with()
+                        .baseUri("https://demoqa.com")
+                        .contentType(ContentType.JSON)
+                        .filter(customLogFilter().withCustomTemplates())
+                        .log().all();
+
+        static public ResponseSpecification resSpec = new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .build();
+    }
+
